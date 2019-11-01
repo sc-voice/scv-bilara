70f5640fff1c822a598594415df3dd6c2c4fdcbb
Karl Lew <karl@firepick.org>
2019-11-01 05:21:44 -0700

    ...

--- scripts/de-trans.js
+++ scripts/de-trans.js
@@ -29,7 +29,7 @@ SYNOPSIS
 
 DESCRIPTION
         Searches bilara-data for sutta and converts it to DE
-        translation file format with wrapped lines
+        translation file format 
 `);
     process.exit(0);
 }
