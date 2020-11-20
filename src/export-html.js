5090f4b870827b0ad92a53a387dd48a10cd629a2
Karl Lew <karl@firepick.org>
2020-11-20 07:26:24 -0800

    export

--- src/export-html.js
+++ src/export-html.js
@@ -14,12 +14,15 @@
             this.linebreak = opts.linebreak || '<br/>\n';
             this.scLinks = opts.scLinks;
             this.html = Object.assign({
-                root: {
-                    verse: ['blockquote','p','strong'],
-                },
-                translation: {
-                    verse: ['blockquote','p'],
-                },
+                verse: [
+                    '<div class="verse" id="$SCID">\n$LINK',
+                ],
+                rootVerse: [
+                    '<p class="pali_verse_paragraph" lang="pi" translate="no">',
+                ],
+                transVerse: [
+                    '<p class="translation_verse_paragraph" lang="en">',
+                ],
             }, opts.html);
 
             let maxResults = this.maxResults = opts.maxResults || 1000;
@@ -64,13 +67,42 @@
             return `<a href="${link}">${linkText}</a>`;
         }
 
+        elementName(elt) {
+            return elt.replace(/< *([^ ]*)(\n|.)*/mi,'$1'); 
+        }
+
         printVerse(verse, author_uid, lang, showPli, showEn) {
+            let {
+                html,
+            } = this;
+            let lines = [];
             if (!verse.length) {
-                return;
+                return lines;
             }
-            showPli && this.printVerseLang(verse, 'pli', undefined);
-            showEn && this.printVerseLang(verse, 'en', 'sujato');
-            this.printVerseLang(verse, lang, author_uid);
+            let scid = verse[0].scid;
+            let suid = scid.split('#')[0];
+            var linkText = new SuttaCentralId(scid).standardForm();
+            let link = this.suttacentralLink(scid, lang, author_uid);
+            let elements = (html.verse || []).map(elt=>{
+                elt = elt.replace(/\$SCID/,scid);
+                elt = elt.replace(/\$LINK/,link);
+                return elt;
+            });
+            if (showPli) {
+                lines = lines.concat(this.printVerseLang(verse, 'pli', undefined));
+            }
+            if (showEn) {
+                liens = lines.concat(this.printVerseLang(verse, 'en', 'sujato'));
+            }
+            lines = lines.concat(this.printVerseLang(verse, lang, author_uid));
+
+            if (lines.length) {
+                let htmlStart = elements.reduce((a,elt)=>`${a}${elt}`, '');
+                let htmlEnd = elements.reduce((a,elt)=>
+                    `</${this.elementName(elt)}>${a}`, '');
+                lines = [htmlStart, ...lines, htmlEnd];
+            }
+            return lines;
         }
 
         printVerseLang(verse, lang, author_uid) {
@@ -79,6 +111,7 @@
                 linebreak,
                 html,
             } = this;
+            let lines = [];
             let scid = verse[0].scid;
             let linkLang = lang === 'pli' ? undefined : lang;
             let isRoot = lang === 'pli'
@@ -94,10 +127,12 @@
                 }
                 return a;
             }, '');
-            let elements = isRoot ? html.root.verse : html.translation.verse;
-            let htmlStart = elements.reduce((a,elt)=>`${a}<${elt}>`, '');
-            let htmlEnd = elements.reduce((a,elt)=>`</${elt}>${a}`, '');
-            text && console.log(`${htmlStart}${text}${htmlEnd}`);
+            let elements = isRoot ? html.rootVerse : html.transVerse;
+            let htmlStart = elements.reduce((a,elt)=>`${a}${elt}`, '');
+            let htmlEnd = elements.reduce((a,elt)=>
+                `</${this.elementName(elt)}>${a}`, '');
+            text && (lines = [ htmlStart, text, htmlEnd, ]);
+            return lines;
         }
 
         exportVerse(res, pattern, n=0) {
@@ -109,32 +144,40 @@
             let showPli = n===2 && searchLang===lang || 
                 n>2 && searchLang!=='pli' && lang!=='pli';
             let showEn = n>2 && searchLang!=='en' && lang!=='en';
+            let lines = [];
             res.mlDocs.forEach(mld => {
-                var suid = mld.suid;
+                let {
+                    suid,
+                    author_uid,
+                } = mld;
                 let segments = mld.segments();
                 let allMatched = segments.reduce((s,a)=>s.matched ? a : false, true);
-                console.log(`dbg allMatched`, {allMatched});
                 let matched = !allMatched;
                 let verse = [];
                 segments.forEach((seg,i) => {
                     var scid = seg.scid;
                     if (/\.1$/.test(scid)) {
-                        matched && this.printVerse(verse, mld.author_uid, 
-                            lang, showPli, showEn);
+                        if (matched) {
+                            lines = lines.concat(
+                                this.printVerse(verse, author_uid, lang, 
+                                    showPli, showEn));
+                        }
                         verse = [];
                         matched = !allMatched;
                     }
                     matched = matched || seg.matched;
                     verse.push(seg);
                     if (i === segments.length && matched) {
-                        this.printVerse(verse, mld.author_uid, lang, showPli, showEn);
+                        lines = lines.concat(
+                            this.printVerse(verse, author_uid, lang, showPli, showEn));
                     }
 
                 });
             });
+            return lines;
         }
 
-        async export(suid) { try {
+        async export(suid, nLang) { try {
             let {
                 bilaraData,
                 seeker: skr,
@@ -149,7 +192,7 @@
             var res = await skr.find(findOpts);
             var secElapsed = (Date.now() - msStart)/1000;
             logger.info(`find() ${secElapsed.toFixed(1)}s`);
-            this.exportVerse(res, suid, 1);
+            return this.exportVerse(res, suid, nLang);
         } catch(e) { 
             this.warn(`export()`, e.message);
             throw e;
--- test/export-html.js
+++ test/export-html.js
@@ -33,6 +33,15 @@
     });
     it("TESTTESTexport(suid)", async()=>{
         let eh = await new ExportHtml().initialize();
-        eh.export('thig4.1');
+        let lines = await eh.export('thig4.1', 2);
+        let length = 45;
+        should(lines.length).equal(length);
+        should.deepEqual(lines.slice(-5),[
+            '<div class=\"verse\" id=\"thig4.1:5.1\">\n<a href=\"https://suttacentral.net/thig4.1/en/sujato#thig4.1:5.1\">Thig4.1:5.1</a>',
+            '<p class=\"pali_verse_paragraph\" lang=\"pi\" translate=\"no\">',
+            '…<br/>\nBhaddā kāpilānī therī ….',
+            '</p>',
+            '</div>',
+        ]);
     });
 });
