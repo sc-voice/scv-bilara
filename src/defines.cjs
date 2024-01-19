19cd568ab81952d382f3e39ae68cdfa754003d3e
Karl Lew <karl@firepick.org>
2024-01-19 10:45:12 -0800

    v3.125.1 += defines

--- src/defines.cjs
+++ src/defines.cjs
@@ -1,8 +1,8 @@
 module.exports.DBG_EXAMPLES   = 0;
-module.exports.DBG_GREP       = true;
+module.exports.DBG_GREP       = 0;
 module.exports.DBG_MLD        = 0;
 module.exports.DBG_FINDARGS   = 0;
-module.exports.DBG_SEEKER     = true;
+module.exports.DBG_SEEKER     = 0;
 module.exports.DBG_TRILINGUAL = 0;
 module.exports.DBG_VERBOSE    = 0;
 
